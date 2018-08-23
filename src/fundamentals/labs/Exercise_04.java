81000ee3b49398f1a9d3fbecbd434ec7e9069b1a
Andy <andymitchell1@live.co.uk>
2018-08-23 13:06:33 +0100

    Submission of Exercise04

--- src/fundamentals/labs/Exercise_04.java
+++ src/fundamentals/labs/Exercise_04.java
@@ -12,7 +12,14 @@ public class Exercise_04 {
 
     public static void main(String[] args) {
 
-        // write code here
+        double radius = 3.14;
+        double height = 5;
+
+        double area = (2 * Math.PI * radius * height) + 2 * Math.PI * radius * radius;
+        double perimeter = 2 * ((2 * radius) + height);
+
+        System.out.println("The area of the cylinder is: " + area);
+        System.out.println("The perimeter of the cylinder is " + perimeter);
 
     }
 }
