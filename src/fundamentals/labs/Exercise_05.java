18779adad604679c8eea8d344bf24cf92a146a7f
Andy <andymitchell1@live.co.uk>
2018-08-25 11:16:22 +0100

    final submission of fundamentals work

--- src/fundamentals/labs/Exercise_05.java
+++ src/fundamentals/labs/Exercise_05.java
@@ -1,5 +1,8 @@
 package fundamentals.labs;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -16,14 +19,26 @@ public class Exercise_05 {
 
     public static void main(String[] args) {
 
-        // create scanner
+        // create scannerdays
         Scanner scanner = new Scanner(System.in);
         // prompt user
         System.out.print("Enter a number in days between 1 and 1,000,000,000: ");
         // assign input to variable as int
         int days = scanner.nextInt();
+        int seconds = days * 86400; // 86400 seconds in a day
+        System.out.println("There are " + seconds + " seconds in " + days + " days.");
+//        BigInteger hoursInDay = new BigInteger("24");
+//        BigInteger minutesinHour  = new BigInteger("60");
+//        BigInteger secondsInMinute  = new BigInteger("60");
+//
+//        BigInteger secondsInDay = new BigInteger("1");
+//        secondsInDay = secondsInDay.multiply(secondsInMinute).multiply(minutesinHour).multiply(hoursInDay);
+//
+//        BigInteger secondsInInput = new BigInteger("1");
+//        secondsInInput = secondsInInput.multiply(secondsInDay);
+//
+//        System.out.println("The number of seconds in " + days + " days is " + secondsInInput);
 
-        // write completed code here
 
     }
 }
