17b0e4ebe7bc47538af0e7c0918dd1642ad7371d
Andy <andymitchell1@live.co.uk>
2018-09-10 22:07:48 +0100

    input_output labs submission

--- src/input_output/labs/Exercise_01.java
+++ src/input_output/labs/Exercise_01.java
@@ -1,5 +1,7 @@
 package input_output.labs;
 
+import java.io.*;
+
 /**
  * Input/Output Exercise 1: File input/output
  *
@@ -8,4 +10,22 @@ package input_output.labs;
  *
  *
  */
+
+class Controller1 {
+    public static void main(String[] args) throws IOException {
+
+            try(FileInputStream input = new FileInputStream("/Users/user/Documents/codingnomads/online_prep/online_java_labs/src/input_output/labs/file.txt");
+            FileOutputStream output = new FileOutputStream("/Users/user/Documents/codingnomads/online_prep/online_java_labs/src/input_output/labs/file_copy.txt");
+            BufferedInputStream in = new BufferedInputStream(input);
+            BufferedOutputStream out = new BufferedOutputStream(output)) {
+
+            int r;
+
+            while ((r = in.read()) != -1) {
+                out.write(r);
+            }
+        }
+
+    }
+}
 
--- src/input_output/labs/Exercise_02.java
+++ src/input_output/labs/Exercise_02.java
@@ -1,5 +1,7 @@
 package input_output.labs;
 
+import java.io.*;
+
 /**
  * Input/Output Exercise 1: File encryption
  *
@@ -12,3 +14,41 @@ package input_output.labs;
  *
  */
 
+class Controller2 {
+    public static void main(String[] args) throws IOException{
+        try(FileReader input = new FileReader("/Users/user/Documents/codingnomads/online_prep/online_java_labs/src/input_output/labs/file2.txt");
+            FileWriter output = new FileWriter("/Users/user/Documents/codingnomads/online_prep/online_java_labs/src/input_output/labs/file2_encrypted.txt");
+            BufferedReader in = new BufferedReader(input);
+            BufferedWriter out = new BufferedWriter(output)) {
+
+            int r;
+
+            while ((r = in.read()) != -1) {
+                if (r == 'a') {
+                    out.write('-');
+                } else if (r == 'e') {
+                    out.write('~');
+                } else {
+                    out.write(r);
+                }
+
+            }
+        }
+
+        try(FileReader input = new FileReader("/Users/user/Documents/codingnomads/online_prep/online_java_labs/src/input_output/labs/file2_encrypted.txt");
+            BufferedReader in = new BufferedReader(input)){
+
+            int r;
+
+            while ((r = in.read()) != -1) {
+                if (r == '-') {
+                    System.out.print('a');
+                } else if (r == '~') {
+                    System.out.print('e');
+                } else {
+                    System.out.print((char)r);
+                }
+            }
+        }
+    }
+}
