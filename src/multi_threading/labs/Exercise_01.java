ccbb11e84b434c063cfd50acc1c3074190477f50
Andy <andymitchell1@live.co.uk>
2018-09-10 22:09:10 +0100

    multi_threading labs

--- src/multi_threading/labs/Exercise_01.java
+++ src/multi_threading/labs/Exercise_01.java
@@ -6,4 +6,28 @@ package multi_threading.labs;
  *      Create an application that creates a Thread using the Runnable interface
  */
 
+class Controller1 {
+    public static void main(String[] args) {
+        RunnableClass runnableClass = new RunnableClass();
+    }
+}
 
+class RunnableClass implements Runnable {
+
+    Thread thread;
+
+    RunnableClass () {
+
+        thread = new Thread(this,"Thread 1");
+
+        thread.start();
+    }
+
+    public void run() {
+        System.out.println(thread.getName() + " starting.");
+
+        System.out.println(thread.getName() +  " running.");
+
+        System.out.println(thread.getName() + " terminating.");
+    }
+}
--- src/multi_threading/labs/Exercise_02.java
+++ src/multi_threading/labs/Exercise_02.java
@@ -1,7 +1,46 @@
 package multi_threading.labs;
 
+import org.omg.PortableServer.THREAD_POLICY_ID;
+
 /**
  * Multithreading Exercise 2:
  *
  *      Create an application that creates a Thread using the Thread class
  */
+
+class Controller2 {
+    public static void main(String[] args) {
+        System.out.println("Start main thread");
+        Thread1 thread1 = new Thread1("Thread 1");
+        thread1.setPriority(1);
+        Thread1 thread2 = new Thread1("Thread 2");
+        thread1.setPriority(2);
+        System.out.println("Stop main thread");
+    }
+}
+
+class Thread1 extends Thread {
+
+    Thread1(String name) {
+        super(name);
+
+        start();
+    }
+
+    @Override
+    public void run() {
+
+
+        System.out.println(getName() + " starting.");
+
+        try {
+            Thread1.sleep(500);
+            System.out.println(getName() + " running.");
+        }
+        catch(Exception e) {
+            System.out.println("Exception caught");
+        }
+
+        System.out.println(getName() + " stopping.");
+    }
+}
