99247cd3118f62196d24009ff2252460b0f445d0
Andy <andymitchell1@live.co.uk>
2018-09-14 12:29:40 +0100

    Generics labs submission

--- src/generics/labs/Exercise_01.java
+++ src/generics/labs/Exercise_01.java
@@ -8,3 +8,51 @@ package generics.labs;
  *          dynamic usage.
  */
 
+class Controller1 {
+    public static void main(String[] args) {
+        int y = 0;
+        double x = 0;
+        String z = "";
+        boolean b = false;
+
+        GenericTest<Double,Integer> genericTest = new GenericTest(x,y);
+
+        GenericTest<String,Boolean> genericTest2 = new GenericTest(z,b);
+
+        genericTest.printDataType();
+
+        genericTest2.printDataType();
+    }
+}
+
+class GenericTest<T,V> {
+
+    T variableOne;
+    V variableTwo;
+
+    public T getVariableOne() {
+        return variableOne;
+    }
+
+    public void setVariableOne(T variableOne) {
+        this.variableOne = variableOne;
+    }
+
+    public V getVaribleTwo() {
+        return variableTwo;
+    }
+
+    public void setVaribleTwo(V varibleTwo) {
+        this.variableTwo = varibleTwo;
+    }
+
+    public GenericTest(T variableOne, V varibleTwo) {
+        this.variableOne = variableOne;
+        this.variableTwo = varibleTwo;
+    }
+
+    public void printDataType() {
+        System.out.println("Variable 1 is type " + variableOne.getClass().getTypeName());
+        System.out.println("Variable 2 is type " + variableTwo.getClass().getTypeName());
+    }
+}
--- src/generics/labs/Exercise_02.java
+++ src/generics/labs/Exercise_02.java
@@ -5,3 +5,19 @@ package generics.labs;
  *
  *      Create a class with a generic method that takes in an array and prints out the value at a certain index.
  */
+
+class Controller2 {
+    public static void main(String[] args) {
+        String[] stringArray = {"Zero","One","Two","Three","Four"};
+
+        GenericArray<String> genericArray = new GenericArray();
+
+        genericArray.getValueAtIndex(stringArray,3);
+    }
+}
+
+class GenericArray <T> {
+    public void getValueAtIndex(T[] array, int index) {
+        System.out.println(array[index]);
+    }
+}
